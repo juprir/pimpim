1d37c8bef757b5e30670274a56803c6dd2e87097
Jupri R <juprir@gmail.com>
2022-06-01 17:33:35 +0700

    Bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pimpim',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Pimpim'),
-        ),
-        body: const Center(
-          child: Text('Welcome to Pimpim App'),
-        ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedNavbar = 0;
+
+  void _changeSelectedNavBar(int index) {
+    setState(() {
+      _selectedNavbar = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pimpim'),
+      ),
+      body: Center(
+        child:
+            Text('Tab aktif: $_selectedNavbar', style: TextStyle(fontSize: 16)),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assignment_turned_in),
+            label: 'Task',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sticky_note_2),
+            label: 'Note',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.date_range),
+            label: 'Appointments',
+          ),
+        ],
+        currentIndex: _selectedNavbar,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        onTap: _changeSelectedNavBar,
       ),
     );
   }
